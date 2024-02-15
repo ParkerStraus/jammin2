02964f07354dbda730945215e661bf00100d4a8a
pestydesperado248 <58351647+pestydesperado248@users.noreply.github.com>
2024-02-15 00:04:44 +0000

    Initial base

---
+++ Assets/MainGameHandler.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using Unity.Burst.CompilerServices;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainGameHandler : MonoBehaviour
+{
+    [Header("UI Elements")]
+    [SerializeField] private TMP_Text p1_Text;
+    [SerializeField] private TMP_Text p2_Text;
+    [SerializeField] private TMP_Text highscore_Text;
+    [SerializeField] private TMP_Text credits_Text;
+    [SerializeField] private TMP_Text gameOver_Text;
+
+
+    [Header("Common Values")]
+    [SerializeField] private int Player1 = 999999;
+    [SerializeField] private int Player2 = 999999;
+    [SerializeField] private int HighScore = 100000;
+    [SerializeField] private int credits = 0;
+    [SerializeField] private bool InGame;
+
+    [SerializeField]
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PrepareGame();
+
+        GetGameInput();
+        SetText();
+    }
+
+    void PrepareGame()
+    {
+        if (Input.GetButtonDown("1P"))
+        {
+            if (!InGame && credits >= 1)
+            {
+                Player1 = 0;
+                Player2 = 0;
+                InGame = true;
+                credits -= 1;
+                SceneManager.LoadSceneAsync(1,LoadSceneMode.Additive);
+            }
+        }
+    }
+
+    void GetGameInput()
+    {
+        if (Input.GetButtonDown("InsertCoin"))
+        {
+            credits += 1;
+        }
+    }
+
+    void SetText()
+    {
+        p1_Text.text = Player1.ToString();
+        p2_Text.text = Player2.ToString();
+        highscore_Text.text = HighScore.ToString();
+        if(InGame == false)
+        {
+            gameOver_Text.enabled = true;
+            if(credits == 0)
+            {
+                credits_Text.text = "INSERT COINS";
+            }
+            else
+            {
+                if(credits == 1) credits_Text.text = "PRESS FOR 1 PLAYER";
+                else credits_Text.text = "PRESS FOR 2 PLAYER";
+            }
+        }
+        else
+        {
+            gameOver_Text.enabled = false;
+            credits_Text.enabled = false;
+
+        }
+    }
+
+    public void AddPointsP1(int points)
+    {
+        Player1 += points;
+    }
+    public void AddPointsP2(int points)
+    {
+        Player2 += points;
+
+    }
+
+    public void StartGame()
+    {
+        InGame = true;
+    }
+
+    public void EndGame()
+    {
+        InGame = false;
+    }
+}
